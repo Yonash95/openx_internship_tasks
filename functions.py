7e668ab50f8b500d2a56467e3cc12725288d7b25
Jonasz Klimala <jonaszklimala@interia.pl>
2022-07-25 10:54:58 +0200

    small changes for exepction in otion 2

--- functions.py
+++ functions.py
@@ -172,12 +172,16 @@ def archive_saved_file():
     date_string = datetime.date.today().strftime("%Y-%m-%d")
     cur_path = pathlib.Path(".")
     paths = cur_path.glob(file_pattern)
-    for path in paths:
-        new_filename = f"{path.stem}_{date_string}{path.suffix}"
-        new_path = cur_path.joinpath(archive_dir, new_filename)
+    try:
+        for path in paths:
+            new_filename = f"{path.stem}_{date_string}{path.suffix}"
+            new_path = cur_path.joinpath(archive_dir, new_filename)
+            path.rename(new_path)
+            print("File archived as '", new_filename, "'")
+    except FileExistsError:
+        print("File was aleready archived today")
 
-        path.rename(new_path)
-        print("File archived as '", new_filename, "'")
+
 
 def load_pandas():
     df = pd.read_json('booking_list.json')
